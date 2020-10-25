579ff82ad0d077c626377d62e945c5ae949eb859
Joe Kaufeld <joe@kenzie.academy>
2020-10-25 17:02:21 -0400

    github pages is very unfriendly to non-jekyll sites
    Signed-off-by: Joe Kaufeld <joe@kenzie.academy>

--- build.py
+++ build.py
@@ -12,8 +12,8 @@ def copy_and_overwrite(from_path, to_path):
 
 from jinja2 import Environment, FileSystemLoader
 
-if not os.path.exists('build'):
-    os.mkdir('build')
+if not os.path.exists('docs'):
+    os.mkdir('docs')
 
 if not os.path.exists('src'):
     print("Cannot find `src` directory!")
@@ -34,14 +34,14 @@ for template in html_files:
         template_str = f.read()
         result = template_loader.from_string(template_str).render()
 
-    with open(f"build/{template}", mode="w") as f:
+    with open(f"docs/{template}", mode="w") as f:
         f.write(result)
 
 for static_file in static:
     with open(f"src/{static_file}") as f:
         template_str = f.read()
 
-    with open(f"build/{static_file}", mode="w") as f:
+    with open(f"docs/{static_file}", mode="w") as f:
         f.write(template_str)
 
-copy_and_overwrite("images", "build/images")
+copy_and_overwrite("images", "docs/images")
